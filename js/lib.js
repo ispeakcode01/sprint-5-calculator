8a2594c031c3e2c6eabd851e173136569462184d
Kateti Mareko <katetib16@gmail.com>
2021-03-02 17:42:25 +1300

    Make clear functional

--- js/lib.js
+++ js/lib.js
@@ -9,12 +9,28 @@ function init() {
 
 function addEventListener() {
     var numberBtns = document.getElementsByClassName('number')
+    var clearBtn = document.getElementById('clear')
+
+    clearBtn.addEventListener('click', clearTerminal)
     for (var i=0; i<numberBtns.length; i++) {
         numberBtns[i].addEventListener('click', handleNumberBtn)
     }
 }
 
+/**
+ * Disply the number that is pressed to the terminal
+ */
 function handleNumberBtn() {
-    var terminal = document.getElementById('terminal')
+    var terminal = getTerminal()
     terminal.value = terminal.value + this.innerHTML
+}
+
+function clearTerminal() {
+    var terminal = getTerminal()
+    terminal.value = ""
+
+}
+
+function getTerminal() {
+    return document.getElementById('terminal')
 }
